ed9beb0aac97c7ac04d970c7e67b217e6f7c9d37
Devin Michael Gray <devingray@Devins-MacBook-Pro.local>
2016-11-18 16:20:53 -0800

    sign-in and sign-out pages. pages restricted to login. conditional menus based on sign-in.

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express'),
     router = express.Router();
 
 // Get Homepage
-router.get('/', function(req, res) {
+router.get('/', ensureAuthenticated, function(req, res) {
     res.render('index');
 });
+
+function ensureAuthenticated(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next();
+    } else {
+        req.flash('success_msg', 'Sign in or create an account.');
+        res.redirect('/users/login');
+    }
+}
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -95,4 +95,10 @@ router.post('/login',
     }
 );
 
+router.get('/logout', function(req, res) {
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+});
+
 module.exports = router;
